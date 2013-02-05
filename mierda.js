c8c7fec8e68de858c7e487e8c9e998d234898104
Jukniz <pc_rey_master@msn.com>
2013-02-05 01:08:00 +0100

    first commit

---
+++ mierda.js
@@ -0,0 +1,21 @@
+function runDemo() {
+  var el = updateTransition();
+  
+  // Configurar un controlador de eventos para invertir la dirección
+  // cuando finalice la transición.
+  
+  el.addEventListener("transitionend", updateTransition, true);
+}
+
+function updateTransition() {
+  var el = document.querySelector("div.izq");
+  
+  if (el) {
+    el.className = "der";
+  } else {
+    el = document.querySelector("div.der");
+    el.className = "izq";
+  }
+  
+  return el;
+}
